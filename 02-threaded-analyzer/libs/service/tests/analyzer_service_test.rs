03caf732cce0e2a429290aa1fb448ab8ad56b007
Vitaly Chouliak <vitaly.chouliak@hotmail.com>
2025-08-16 16:53:00 +0200

    Add test for processing in chunks

--- 02-threaded-analyzer/libs/service/tests/analyzer_service_test.rs
+++ 02-threaded-analyzer/libs/service/tests/analyzer_service_test.rs
@@ -2,6 +2,7 @@ use std::sync::{Arc, Mutex};
 
 use ta_core::traits::{Job, ThreadPool};
 use service::analyzer::AnalyzerService;
+use infrastructure::threads::WorkerThreadPool;
 
 struct FakePool;
 impl ThreadPool for FakePool {
@@ -33,4 +34,30 @@ fn processes_jobs_in_order_with_chunking() {
 
     let got = results.lock().unwrap().clone();
     assert_eq!(got, vec![1, 2, 3, 4, 5]);
+}
+
+#[test]
+fn analyzer_service_processes_in_chunks() {
+    let pool = WorkerThreadPool::new(2);
+    let service = AnalyzerService::new(&pool, 3);
+
+    let result = Arc::new(Mutex::new(Vec::new()));
+
+    let jobs: Vec<Box<dyn Job>> = (0..9)
+        .map(|i| {
+            let result_clone = Arc::clone(&result);
+            Box::new(move || {
+                let mut data = result_clone.lock().unwrap();
+                data.push(i);
+            }) as Box<dyn Job>
+        })
+        .collect();
+
+    service.process(jobs);
+
+    std::thread::sleep(std::time::Duration::from_millis(500));
+
+    let mut data = result.lock().unwrap();
+    data.sort();
+    assert_eq!(*data, (0..9).collect::<Vec<_>>());
 }
